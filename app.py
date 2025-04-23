c894715cca926f9ec02beefd2a4db3ddc06728ad
Jayaragul <jayaragul123@gmail.com>
2025-04-23 23:01:21 +0530

    mhvnb

--- app.py
+++ app.py
@@ -43,5 +43,7 @@ def get_diagnosis():
     return jsonify({"response": response})
 
 # Run the Flask app
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
